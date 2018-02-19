b476dfaf9331fddc9d3160f46e7b385ae541f817
Alexandre Oliveira <rockytvbr@gmail.com>
2018-02-18 22:07:27 -0300

    Add error handling

--- examples/test.rs
+++ examples/test.rs
@@ -2,5 +2,5 @@ extern crate eden;
 
 fn main() {
     let client = eden::Eden::new();
-    println!("{:?}", client.character().get_public_information(&client, 2112704025));
+    println!("{:?}", client.character().get_public_information(&client, 0));
 }
--- src/character.rs
+++ src/character.rs
@@ -16,28 +16,37 @@ pub struct Character {
 
 impl Character {
     pub fn get_public_information(&self, client: &super::Eden, id: u32) -> Result<Self, String> {
-        let response = client.get(&format!("https://esi.tech.ccp.is/latest/characters/{character_id}/?datasource=tranquility", character_id=id)).send();
+        let response = client.get(&format!("characters/{character_id}", character_id=id)).send();
 
         match response {
             Ok(mut data) => {
-                match data.json::<serde_json::Value>() {
-                    Ok(c) => {
-                        Ok(Character {
-                            alliance_id: match c["alliance_id"].as_u64() {
-                                Some(id) => id,
-                                None => Default::default()
-                            },
-                            ancestry_id: c["ancestry_id"].as_u64().unwrap(),
-                            birthday: c["birthday"].as_str().unwrap().to_string(),
-                            bloodline_id: c["bloodline_id"].as_u64().unwrap(),
-                            corporation_id: c["corporation_id"].as_u64().unwrap(),
-                            description: c["description"].as_str().unwrap().to_string(),
-                            gender: c["gender"].as_str().unwrap().to_string(),
-                            name: c["name"].as_str().unwrap().to_string(),
-                            security_status: c["security_status"].as_f64().unwrap(),
-                        })  
-                    },
-                    Err(_) => Err(format!("error parsing json"))
+                if data.status().is_success() {
+                    match data.json::<serde_json::Value>() {
+                        Ok(c) => {
+                            Ok(Character {
+                                alliance_id: match c["alliance_id"].as_u64() {
+                                    Some(id) => id,
+                                    None => Default::default()
+                                },
+                                ancestry_id: c["ancestry_id"].as_u64().unwrap(),
+                                birthday: c["birthday"].as_str().unwrap().to_string(),
+                                bloodline_id: c["bloodline_id"].as_u64().unwrap(),
+                                corporation_id: c["corporation_id"].as_u64().unwrap(),
+                                description: c["description"].as_str().unwrap().to_string(),
+                                gender: c["gender"].as_str().unwrap().to_string(),
+                                name: c["name"].as_str().unwrap().to_string(),
+                                security_status: c["security_status"].as_f64().unwrap(),
+                            })
+                        },
+                        Err(_) => Err(format!("error parsing json"))
+                    }
+                } else if data.status().is_server_error() || data.status().is_client_error() {
+                    match data.json::<serde_json::Value>() {
+                        Ok(err) => Err(err["error"].as_str().unwrap().to_string()),
+                        Err(_) => Err(format!("error parsing json, status: {:?}", data.status()))
+                    }
+                } else {
+                    Err(format!("something bad happened, status: {:?}", data.status()))
                 }
             },
             Err(_) => Err(format!("something bad happened"))
